1ce907fe0504454b55e0ccb02be21bc71c38e50b
excel <axceldesigns@gmail.com>
2025-08-27 14:37:59 +0100

    Final project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,17 +15,24 @@ const navLinks: NavLinkProps[] = [
 
 export default function Header() {
   return (
-    <header>
+    <header className="max-md:p-2 p-4">
       <div className="flex flex-row justify-between items-center ">
-        <nav className="flex flex-row gap-4">
+        <nav className="hidden sm:flex flex-row gap-4">
           {navLinks.map((link) => (
             <NavLink key={link.path} to={link.path} > {link.label}</NavLink>
           ))}
         </nav>
-        <div>
+        <div className="sm:hidden">
+          <i className=" fa-solid fa-bars fa-xl"></i>
+        </div>
+        <div className="w-10">
           <img src={logo} alt="" />
         </div>
-        <div></div>
+        <div className="flex flex-row gap-2">
+          <i className=" fa-regular fa-heart"></i>
+          <i className=" fa-solid fa-cart-shopping"></i>
+          <i className=" fa-solid fa-user"></i>
+        </div>
       </div>
       <div></div>
     </header>
